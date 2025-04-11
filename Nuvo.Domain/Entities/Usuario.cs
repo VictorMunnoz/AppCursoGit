3ff3ffabf63d5de8eada7dba5ed62d187af6a1d8
Victor Jr Gallegos <victorgalmun@gmail.com>
2025-04-11 11:00:55 -0600

    Mi primer commit

---
+++ Nuvo.Domain/Entities/Usuario.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Nuvo.Domain.Entities
+{
+
+    //Se agrega comentario: Víctor Gallegos
+    internal class Usuario
+    {
+        [Key]     
+        public Guid Id { get; set; }
+
+        [StringLength(300)]
+        [Required]
+        public required string Nombre { get; set; }
+
+        [StringLength(300)]
+        [Required]
+        public required string Apellido { get; set; }
+
+        [StringLength(300)]
+        [Required]
+        public required string NombreDeUsuario { get; set; }
+
+        [StringLength(300)]
+        public string? Puesto { get; set; }
+
+        public string GetInfo() => $"Id: {Id} Nombre: {Nombre} Apellido: {Apellido} NombreDeUsuario: {NombreDeUsuario}";
+    }
+}
---
+++ Nuvo.Infraestructure/Persistence/NuvoDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nuvo.Infraestructure.Persistence
+{
+    public class NuvoDbContext : DbContext
+    {
+
+        DbSet<Domain.Entities.Usuario> Usuarios { get; set; }
+    }
+}
