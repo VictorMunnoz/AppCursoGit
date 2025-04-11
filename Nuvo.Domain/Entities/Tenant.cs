f93a6455639c4a34e08ad1d47f30da6737d5024b
Victor Jr Gallegos <victorgalmun@gmail.com>
2025-04-11 12:39:54 -0600

    Se agrega entidad de Tenant

---
+++ Nuvo.Domain/Entities/Tenant.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Nuvo.Domain.Entities
+{
+    public class Tenant
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [StringLength(200)]
+        public required string Nombre { get; set; }
+    }
+}
